6d4821c08cb2a6845512618922be45d2155dd098
lobosfybr <87049169+lobosfybr@users.noreply.github.com>
2021-07-29 15:38:11 -0400

    Add files via upload

---
+++ Assignment 1/script.js
@@ -0,0 +1,54 @@
+var userName="Kenny";
+var userAge="25";
+var userName2="Rob";
+var userAge2="33";
+var userName3="Ann";
+var userAge3="22";
+var userName4="Leo";
+var userAge4="26";
+var userName5="Denise";
+var userAge5="45";
+var userName6="Cloud";
+var userAge6="23";
+var userName7="Liane";
+var userAge7="26";
+var userName8="Bob";
+var userAge8="24";
+var userName9="Tron";
+var userAge9="31";
+var userName10="Kate";
+var userAge10="21";
+
+document.write(`
+    <p><b>Name: </b>${userName}
+    <br>
+    <b>Age: </b>${userAge}</p>
+    <p><b>Name: </b>${userName2}
+    <br>
+    <b>Age: </b>${userAge2}</p>
+    <p><b>Name: </b>${userName3}
+    <br>
+    <b>Age: </b>${userAge3}</p>
+    <p><b>Name: </b>${userName4}
+    <br>
+    <b>Age: </b>${userAge4}</p>
+    <p><b>Name: </b>${userName5}
+    <br>
+    <b>Age: </b>${userAge5}</p>
+    <p><b>Name: </b>${userName6}
+    <br>
+    <b>Age: </b>${userAge6}</p>
+    <p><b>Name: </b>${userName7}
+    <br>
+    <b>Age: </b>${userAge7}</p>
+    <p><b>Name: </b>${userName8}
+    <br>
+    <b>Age: </b>${userAge8}</p>
+    <p><b>Name: </b>${userName9}
+    <br>
+    <b>Age: </b>${userAge9}</p>
+    <p><b>Name: </b>${userName10}
+    <br>
+    <b>Age: </b>${userAge10}</p>
+    
+`)
